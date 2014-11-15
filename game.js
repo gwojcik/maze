a62fdd123e78dbbfd149068340eb54e1912d9dd1
Grzegorz Wójcik <grzegorz.w.1597@gmail.com>
2014-11-15 15:03:52 +0100

    added player and exit position

--- game.js
+++ game.js
@@ -24,6 +24,7 @@ for the JavaScript code in this page.
 */
 
 function Game() {
+   "use strict";
 }
 
 Game.prototype.init = function() {
@@ -33,15 +34,60 @@ Game.prototype.init = function() {
 
    var time = new Date();
    this.timeStart = time.getTime();
+   this.lastFrameTime = time.getTime();
 
+   this.player = {
+      pos: {
+         x: 0.0,
+         y: 0.0
+      },
+      v: {
+         x: 0.0,
+         y: 0.0
+      }
+   };
+   this.exitPos = {
+      x: 0.0,
+      y: 0.0
+   }
+   this.mazeSeed = 0;
+   this.keyInput = {};
    
    this.mazeShader = this.graphic.loadProgramFile("./simple.vert","./maze.frag");
    this.mazeShaderSeed = this.graphic.gl.getUniformLocation(this.mazeShader, 'seed');
+   this.mazePlayerPos = this.graphic.gl.getUniformLocation(this.mazeShader, 'playerPos');
+   this.mazeExitPos = this.graphic.gl.getUniformLocation(this.mazeShader, 'exitPos');
    this.graphic.gl.useProgram(this.mazeShader);
 
    this.genNewMaze = true;
 
    this.draw();
+}
+
+Game.prototype.keyDownEvent = function(event) {
+   var key = event.keyCode;
+   if (key == 0x26 || key == 0x57) { //ArrowUp || W
+      this.keyInput.up = true;
+   } else if (key == 0x28 || key == 0x53) { //ArrowDown || S
+      this.keyInput.down = true;
+   } else if (key == 0x27 || key == 0x44) { //ArrowRight || D
+      this.keyInput.right = true;
+   } else if (key == 0x25 || key == 0x41) { //ArrowRight || A
+      this.keyInput.left = true;
+   }
+}
+
+Game.prototype.keyUpEvent = function(event) {
+   var key = event.keyCode;
+   if (key == 0x26 || key == 0x57) { //ArrowUp || W
+      this.keyInput.up = false;
+   } else if (key == 0x28 || key == 0x53) { //ArrowDown || S
+      this.keyInput.down = false;
+   } else if (key == 0x27 || key == 0x44) { //ArrowRight || D
+      this.keyInput.right = false;
+   } else if (key == 0x25 || key == 0x41) { //ArrowRight || A
+      this.keyInput.left = false;
+   }
 }
 
 Game.prototype.draw = function() {
@@ -51,17 +97,71 @@ Game.prototype.draw = function() {
       that.draw();
    };
    var time = (new Date()).getTime();
-	this.graphic.gl.uniform1f(this.mazeShaderSeed, ((time/1000) & 0x3FF) );
-   this.graphic.draw();
-   var P = new Uint8Array(4 * this.graphic.size.x * this.graphic.size.y);
-   this.graphic.gl.readPixels(0, 0, this.graphic.size.x, this.graphic.size.y, this.graphic.gl.RGBA, this.graphic.gl.UNSIGNED_BYTE, P);
+
+   var thisFrameTime = time;
+   var frameTime = document.getElementById('Frame');
+   frameTime.value = thisFrameTime - this.lastFrameTime;
+   this.lastFrameTime = thisFrameTime;
 
    if(this.genNewMaze) {
+      this.mazeSeed = ((time/1000) & 0x3FF);
+      this.player.pos = {x: 0, y:0};
+      this.exitPos = {x: 0, y:0};
+      this.updateGraphic();
+      this.graphic.draw();
+      var P = new Uint8Array(4 * this.graphic.size.x * this.graphic.size.y);
+      this.graphic.gl.readPixels(0, 0, this.graphic.size.x, this.graphic.size.y, this.graphic.gl.RGBA, this.graphic.gl.UNSIGNED_BYTE, P);
       this.addStartAndExit(P);
       this.genNewMaze = false;
+   } else {
+      this.updatePlayer();
+      this.updateGraphic();
+      this.graphic.draw();
    }
 
    window.requestAnimationFrame(callback,this.canvas);
+}
+
+Game.prototype.updatePlayer = function() {
+   "use strict";
+   var x = 0;
+   var y = 0;
+   if (this.keyInput.up) {
+      y += 1;
+   }
+   if (this.keyInput.down) {
+      y -= 1;
+   }
+   if (this.keyInput.right) {
+      x += 1;
+   }
+   if (this.keyInput.left) {
+      x -= 1;
+   }
+
+   var len = Math.sqrt(x*x + y*y);
+
+   if (len > 0) {
+      this.player.v.x += (x/len) * 0.01;
+      this.player.v.y += (y/len) * 0.01;
+      var vLen = Math.sqrt(this.player.v.x * this.player.v.x + this.player.v.y * this.player.v.y);
+      if ( vLen > 1) {
+         this.player.v.x /= vLen;
+         this.player.v.x /= vLen;
+      }
+   } else {
+      this.player.v.x *= 0.9;
+      this.player.v.y *= 0.9;
+   }
+   this.player.pos.x += this.player.v.x * 0.1;
+   this.player.pos.y += this.player.v.y * 0.1;
+}
+
+Game.prototype.updateGraphic = function() {
+   "use strict";
+	this.graphic.gl.uniform1f(this.mazeShaderSeed, this.mazeSeed );
+	this.graphic.gl.uniform2f(this.mazePlayerPos, this.player.pos.x, this.player.pos.y );
+	this.graphic.gl.uniform2f(this.mazeExitPos, this.exitPos.x, this.exitPos.y );
 }
 
 Game.prototype.dijkstra = function(maze, start, end) {
@@ -112,7 +212,7 @@ Game.prototype.dijkstra = function(maze, start, end) {
    } else {
       for(var i = 0; i< this.graphic.size.x * this.graphic.size.y; i++) {
          var x = i%this.graphic.size.x;
-         var y = Math.floor(i/this.graphic.size.x);
+         var y = this.graphic.size.y - Math.floor(i/this.graphic.size.x);
          var color = P[i]%256;
          ctx.fillStyle="rgb(" + color + ',' + color + ',' + color + ')';
          ctx.fillRect(x, y, 1, 1);
@@ -120,7 +220,7 @@ Game.prototype.dijkstra = function(maze, start, end) {
       var v = end;
       ctx.fillStyle="rgb(255,0,0)";
       while (v.x != start.x || v.y != start.y) {
-         ctx.fillRect(v.x, v.y, 1, 1);
+         ctx.fillRect(v.x, this.graphic.size.y - v.y, 1, 1);
          var minValue = 8*1024;
          var minID = 0;
          var vn;
@@ -164,4 +264,19 @@ Game.prototype.addStartAndExit = function(maze) {
          len.y = start.y - end.y;
       } while (Math.sqrt(len.x*len.x + len.y*len.y) < 200)
    } while (!this.dijkstra(maze, start, end))
+
+   function convertPos(pos, imageSize){
+      var x = pos.x;
+      var y = pos.y;
+      x /= imageSize.x;
+      y /= imageSize.y;
+      y *= (imageSize.y/imageSize.x);
+      x *= 2*20.0;
+      y *= 2*20.0;
+      return {x: x, y: y};
+   }
+   console.log(this);
+
+   this.player.pos = convertPos(start, this.graphic.size);
+   this.exitPos = convertPos(end, this.graphic.size);
 }
--- maze.frag
+++ maze.frag
@@ -14,9 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 precision highp float;
 varying highp vec2 uv;
 uniform float seed;
+uniform vec2 playerPos;
+uniform vec2 exitPos;
 
 float BBS1(float x) {
    return mod((x * x), 16807.0) * (2.0/16807.0) - 1.0;
@@ -42,7 +45,15 @@ vec2 ComplexMul(in vec2 A, in vec2 B)
 void main() {
    float aspect = (3.0/4.0);
    gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0);
-   vec2 pos = (uv*vec2(1.0, aspect)+vec2(1.0,1.0))*20.0;
+   vec2 pos = ((uv+vec2(1.0, 1.0))*vec2(1.0, aspect))*20.0;
+   if ( distance(pos, playerPos) < 0.2) {
+      gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0);
+      return;
+   }
+   if ( distance(pos, exitPos) < 0.5) {
+      gl_FragColor = vec4( 0.0, 0.0, 1.0, 1.0);
+      return;
+   }
    pos = ComplexMul(normalize(vec2(1.0, 1.0)), pos);
    vec2 tile = ceil(pos);
    vec2 p = fract(pos);
